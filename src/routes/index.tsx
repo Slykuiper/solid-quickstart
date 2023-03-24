4e9c954bb3725e063abb863e6215b19fc13531fb
Corbin scott <Slykuiper@gmail.com>
2023-03-24 02:25:44 -0400

    231

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <a href="https://start.solidjs.com" target="_blank">
           start.solidjs.com
         </a>{" "}
-        to learn how to build SolidStart apps.
+        to learn how to build slykuiper apps.
       </p>
     </main>
   );
